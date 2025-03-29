7f610c1e62c48f6508c6e01dd9466d42a8426b48
file not found <voodoologic@users.noreply.github.com>
2025-03-29 16:03:10 -0700

    fix for 6.1 and embroider

--- app/app.js
+++ app/app.js
@@ -2,6 +2,14 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'dkhtodo-frontend/config/environment';
+// all this is because ember inpector breaks with
+// the use of embroider
+import Ember from 'ember';
+import * as runtime from '@glimmer/runtime';
+import * as tracking from '@glimmer/tracking';
+import * as validator from '@glimmer/validator';
+import * as reference from '@glimmer/reference';
+import {RSVP} from '@ember/-internals/runtime';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
@@ -10,3 +18,12 @@ export default class App extends Application {
 }
 
 loadInitializers(App, config.modulePrefix);
+window.define('@glimmer/tracking', () => tracking);
+window.define('@glimmer/runtime', () => runtime);
+window.define('@glimmer/validator', () => validator);
+window.define('@glimmer/reference', () => reference);
+window.define('rsvp', () => RSVP);
+window.define('ember', () => ({ default: Ember }));
+window.define('dkhtodo-frontend/config/environment', () => ({
+  default: config,
+}));
